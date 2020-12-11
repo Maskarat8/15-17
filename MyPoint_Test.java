95d6e82331385dda848a4303be05dbd3bb9ea005
Maskarat8 <71455730+Maskarat8@users.noreply.github.com>
2020-12-11 13:50:57 +0200

    Add files via upload

---
+++ MyPoint_Test.java
@@ -0,0 +1,17 @@
+package conko1;
+
+public class MyPoint_Test {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		
+		MyPoint a = new MyPoint();
+		
+		MyPoint b = new MyPoint(10, 30.5);
+		
+		
+		System.out.println(a.distance(b));
+
+	}
+
+}
